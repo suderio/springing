f9bf4d81dd77f0b52aa252df284d041a7c8b8ca2
Paulo Suderio <paulo.suderio@gmail.com>
2019-02-10 23:38:17 +0000

    Creating site for 0.1.0

--- clover/rest/AccountRepository.js
+++ clover/rest/AccountRepository.js
@@ -4,7 +4,7 @@ var clover = new Object();
 clover.pageData = {"classes":[{"el":52,"id":0,"methods":[{"el":37,"sc":3,"sl":31}],"name":"AccountRepository","sl":24},{"el":51,"id":4,"methods":[{"el":46,"sc":5,"sl":43},{"el":50,"sc":5,"sl":48}],"name":"AccountRepository.Accounts","sl":39}]}
 
 // JSON: {test_ID : {"methods": [ID1, ID2, ID3...], "name" : "testXXX() void"}, ...};
-clover.testTargets = {"test_3":{"methods":[{"sl":31},{"sl":43},{"sl":48}],"name":"shouldQueryEntity","pass":true,"statements":[{"sl":33},{"sl":35},{"sl":36},{"sl":45},{"sl":49}]}}
+clover.testTargets = {"test_1":{"methods":[{"sl":31},{"sl":43},{"sl":48}],"name":"shouldQueryEntity","pass":true,"statements":[{"sl":33},{"sl":35},{"sl":36},{"sl":45},{"sl":49}]}}
 
 // JSON: { lines : [{tests : [testid1, testid2, testid3, ...]}, ...]};
-clover.srcFileLines = [[], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [3], [], [3], [], [3], [3], [], [], [], [], [], [], [3], [], [3], [], [], [3], [3], [], [], []]
+clover.srcFileLines = [[], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [1], [], [1], [], [1], [1], [], [], [], [], [], [], [1], [], [1], [], [], [1], [1], [], [], []]
--- clover/rest/Application.js
+++ clover/rest/Application.js
@@ -1,10 +1,10 @@
 var clover = new Object();
 
 // JSON: {classes : [{name, id, sl, el,  methods : [{sl, el}, ...]}, ...]}
-clover.pageData = {"classes":[{"el":19,"id":8,"methods":[{"el":17,"sc":3,"sl":15}],"name":"Application","sl":13}]}
+clover.pageData = {"classes":[{"el":13,"id":8,"methods":[{"el":11,"sc":3,"sl":9}],"name":"Application","sl":7}]}
 
 // JSON: {test_ID : {"methods": [ID1, ID2, ID3...], "name" : "testXXX() void"}, ...};
 clover.testTargets = {}
 
 // JSON: { lines : [{tests : [testid1, testid2, testid3, ...]}, ...]};
-clover.srcFileLines = [[], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], []]
+clover.srcFileLines = [[], [], [], [], [], [], [], [], [], [], [], [], [], []]
